73dd7c6a6621fe2126b6ac7628a4b605518041a4
James Michael Brascome <Brascome@uab.edu>
2022-01-30 13:50:57 -0600

    initial checkin

---
+++ uber_ocr_solution/uber_ocr/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace uber_ocr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 67);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(752, 345);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ uber_ocr_solution/uber_ocr/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using IronOcr;
+
+namespace uber_ocr
+{
+    public partial class Form1 : Form
+    {
+        string strFolder = @"C:\Users\mike\OneDrive\Documents\School\CS_685_Foundations_of_Data_Science\Homework\Uber_Trip_Annotation\BRASCOME";
+        string strOutFile = @"C:\Users\mike\OneDrive\Documents\School\CS_685_Foundations_of_Data_Science\Homework\Uber_Trip_Annotation\out_file.csv";
+        string strHeader = "Image Filename, Origin Address, Origin Coordinates, Destination Address, Destination Coordinates, Fare, Duration, Distance, Vehicle Type, Time Requested, Date Requested, Points Earned, Origin Coordinates, Coordinates 2, Coordinates 3,	Coordinates 4, Coordinates 5, Coordinates 6, Coordinates 7, Coordinates 8, Coordinates 9, Coordinates 10, Coordinates 11, Coordinates 12, Coordinates 13, Coordinates 14, Coordinates 15, Coordinates 16, Coordinates 17, Coordinates 18, Coordinates 19, Coordinates 20";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string strErrMsg = string.Empty;
+            try
+            {
+                if (process_files(strFolder, strOutFile, ref strErrMsg))
+                {
+                    //done
+                    this.displayMsg("Done!");
+                }
+                else
+                {
+                    this.handleError(strErrMsg);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.handleError(ex.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Loop through all files and process each one
+        /// </summary>
+        /// <param name="strFolder"></param>
+        /// <param name="strOutFile"></param>
+        /// <param name="strErrMsg"></param>
+        /// <returns></returns>
+        private bool process_files(string strFolder, string strOutFile, ref string strErrMsg)
+        {
+            bool blnRetVal = true;
+            string strOcrText = string.Empty;
+
+            try
+            {
+                string[] files = System.IO.Directory.GetFiles(strFolder, "*.jpg");
+
+                foreach (string strFile in files)
+                {
+                    // get the ocr string from the file
+                    if (this.read_ocr(strFile, ref strOcrText, ref strErrMsg))
+                    {
+                        string strCSV = string.Empty;
+                        if (this.parse_ocr_text(strOcrText, strFile, ref strCSV, ref strErrMsg))
+                        {
+
+                        }
+                        else
+                        {
+                            // an error happened whie parsing the ocr text
+                            this.handleError(strErrMsg);
+                        }
+                    }
+                    else
+                    {
+                        // an error happened while reading the ocr data
+                        this.handleError(strErrMsg);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                blnRetVal = false;
+                strErrMsg = ex.ToString();
+            }
+            return blnRetVal;
+        }
+
+        /// <summary>
+        /// Read text with ocr and return the text
+        /// </summary>
+        /// <param name="strImg"></param>
+        /// <param name="strText"></param>
+        /// <param name="strErrMsg"></param>
+        /// <returns></returns>
+        private bool read_ocr(string strImg, ref string strText, ref string strErrMsg)
+        {
+            bool blnRetVal = true;
+            try
+            {
+                var Ocr = new IronTesseract();
+                using (var Input = new OcrInput(strImg))
+                {
+                    // Input.Deskew();  // use if image not straight
+                    // Input.DeNoise(); // use if image contains digital noise
+                    var Result = Ocr.Read(Input);
+                    this.txtOutput.Text = Result.Text;
+                    Console.WriteLine(Result.Text);
+                    strText = Result.Text;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                blnRetVal = false;
+                strErrMsg = ex.ToString();
+            }
+            return blnRetVal;
+        }
+
+        /// <summary>
+        /// parse the text from the ocr string and return a csv line
+        /// </summary>
+        /// <param name="strInput"></param>
+        /// <param name="strFile"></param>
+        /// <param name="strCSV"></param>
+        /// <param name="strErrMsg"></param>
+        /// <returns></returns>
+        private bool parse_ocr_text(string strInput, string strFile, ref string strCSV, ref string strErrMsg)
+        {
+            bool blnRetVal = true;
+
+            string strImageFilename = string.Empty; 
+            string strOriginAddress = string.Empty; 
+            string strOriginCoordinates = string.Empty; 
+            string strDestinationAddress = string.Empty; 
+            string strDestinationCoordinates = string.Empty; 
+            string strFare = string.Empty; 
+            string strDuration = string.Empty; 
+            string strDistance = string.Empty; 
+            string strVehicleType = string.Empty; 
+            string strTimeRequested = string.Empty; 
+            string strDateRequested = string.Empty; 
+            string strPointsEarned = string.Empty; 
+            string strCoordinates2 = string.Empty; 
+            string strCoordinates3 = string.Empty; 
+            string strCoordinates4 = string.Empty; 
+            string strCoordinates5 = string.Empty; 
+            string strCoordinates6 = string.Empty; 
+            string strCoordinates7 = string.Empty; 
+            string strCoordinates8 = string.Empty; 
+            string strCoordinates9 = string.Empty; 
+            string strCoordinates10 = string.Empty; 
+            string strCoordinates11 = string.Empty; 
+            string strCoordinates12 = string.Empty; 
+            string strCoordinates13 = string.Empty; 
+            string strCoordinates14 = string.Empty; 
+            string strCoordinates15 = string.Empty; 
+            string strCoordinates16 = string.Empty; 
+            string strCoordinates17 = string.Empty; 
+            string strCoordinates18 = string.Empty; 
+            string strCoordinates19 = string.Empty; 
+            string strCoordinates20 = string.Empty;
+            try
+            {
+                
+                string[] strLines = strInput.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                foreach(string strLine in strLines)
+                {
+                    this.displayMsg(strLine);
+                    string strOutLine = string.Format("Image Filename, Origin Address, Origin Coordinates, Destination Address, Destination Coordinates, Fare, Duration, Distance, Vehicle Type, Time Requested, Date Requested, Points Earned, Origin Coordinates, Coordinates 2, Coordinates 3,	Coordinates 4, Coordinates 5, Coordinates 6, Coordinates 7, Coordinates 8, Coordinates 9, Coordinates 10, Coordinates 11, Coordinates 12, Coordinates 13, Coordinates 14, Coordinates 15, Coordinates 16, Coordinates 17, Coordinates 18, Coordinates 19, Coordinates 20", strImageFilename, strOriginAddress, strOriginCoordinates, strDestinationAddress, strDestinationCoordinates, strFare, strDuration, strDistance, strVehicleType, strTimeRequested, strDateRequested, strPointsEarned, strOriginCoordinates, strCoordinates2, strCoordinates3, strCoordinates4, strCoordinates5, strCoordinates6, strCoordinates7, strCoordinates8, strCoordinates9, strCoordinates10, strCoordinates11, strCoordinates12, strCoordinates13, strCoordinates14, strCoordinates15, strCoordinates16, strCoordinates17, strCoordinates18, strCoordinates19, strCoordinates20);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                blnRetVal = false;
+                strErrMsg = ex.ToString();
+            }
+            return blnRetVal;
+        }
+
+        /// <summary>
+        /// write a csv line to the out file
+        /// </summary>
+        /// <param name="strOutFile"></param>
+        /// <param name="strCSV"></param>
+        /// <param name="strErrMsg"></param>
+        /// <returns></returns>
+        private bool write_to_file(string strOutFile, string strCSV, ref string strErrMsg)
+        {
+            bool blnRetVal = true;
+            try
+            {
+                System.IO.File.WriteAllText(strOutFile, strCSV);
+            }
+            catch (Exception ex)
+            {
+                blnRetVal = false;
+                strErrMsg = ex.ToString();
+            }
+            return blnRetVal;
+        }
+
+        /// <summary>
+        /// display user messages to the ui
+        /// </summary>
+        /// <param name="strMsg"></param>
+        private void displayMsg(string strMsg)
+        {
+            this.txtOutput.AppendText(string.Format("{0}\r\n", strMsg));
+        }
+
+        /// <summary>
+        /// display error messages to the ui
+        /// </summary>
+        /// <param name="strMsg"></param>
+        private void handleError(string strMsg)
+        {
+            this.txtOutput.AppendText(string.Format("Error {0}\r\n", strMsg));
+        }
+    
+    }
+}
